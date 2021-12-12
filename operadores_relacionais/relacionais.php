511c7d284c25ab788226191ace9ad0e13f07c6f6
Gustavo Backes Kitzbeger <89171204+Gustavobackes@users.noreply.github.com>
2021-12-12 17:31:15 -0300

    Update relacionais.php

--- operadores_relacionais/relacionais.php
+++ operadores_relacionais/relacionais.php
@@ -10,8 +10,22 @@
 <body>
   <div>
     <?php
+$n1 = $_GET["a"];
+    $n2 = $_GET["b"];
+    $tipo = $_GET["op"];
+    echo "<h2>Os valores passados foram $n1 e $n2 <br></h2>";
+    $r = ($tipo == "s") ? $n1 + $n2 : $n1 * $n2; //Se "$tipo" for "s", acontece a soma, senao,a multiplicação
+    echo "$r<br>";
 
-    echo "ola";
+    //Igual & Identico
+    $a = 3;
+    $b = "3";
+    $r = ($a == $b) ? "sim" : "não"; //O igual == não considera o tipo
+    echo "<br/>A variavel A é igual a B? $r";
+    $a = 3;
+    $b = "3";
+    $r = ($a === $b) ? "sim" : "não"; //O identico === considera o tipo
+    echo "<br>A variavel A é igual a B? $r";
     ?>
   </div>
 </body>
