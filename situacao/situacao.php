0fe66989003c34864a7d7a30214c6c1fe7db434b
Gustavo Backes Kitzbeger <89171204+Gustavobackes@users.noreply.github.com>
2021-12-12 18:33:08 -0300

    Add files via upload

---
+++ situacao/situacao.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="_css2/estilo2.css">
+  <title>Media</title>
+</head>
+
+<body>
+  <div>
+    <?php
+    $n1 = $_GET["n"];
+    $n2 = $_GET["m"];
+    $m = ($n1 + $n2) / 2;
+    echo "As notas 1 e 2 são repectivamente: $n1 e $n2";
+    echo "<br>E a média é: $m";
+
+    echo "<br>O aluno foi " . (($m < 6) ? "Reprovado" : "Aprovado");
+    ?>
+  </div>
+</body>
+
+</html>
